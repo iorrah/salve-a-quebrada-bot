63380af6e9dbecbbba3237a85db44358df4b1f20
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-04-29 17:22:45 +0200

    Wrap shuffle logic inside method

--- shuffle.js
+++ shuffle.js
@@ -3,34 +3,6 @@ var path = require('path');
 
 var PATH_TARGET = 'db.json';
 
-try {
-  var stringJSON = fs.readFileSync(PATH_TARGET, 'utf-8');
-} catch(err) {
-  console.log(err);
-}
-
-var listJSON = JSON.parse(stringJSON);
-
-if (!listJSON.stores.length) {
-  console.log('Array is empty');
-  return;
-}
-
-console.log('Target has ' + listJSON.stores.length + ' items');
-var shuffledItems = shuffle(listJSON.stores);
-var shuffledJSON = { stores: shuffledItems };
-
-if (fs.existsSync(PATH_TARGET)) {
-  var stringShuffledJSON = JSON.stringify(shuffledJSON);
-
-  try {
-    fs.writeFileSync(PATH_TARGET, stringShuffledJSON);
-    console.log('Done');
-  } catch(err) {   
-    console.log(err);
-  };
-}
-
 function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -45,3 +17,37 @@ function shuffle(array) {
 
   return array;
 }
+
+function startShuffle() {
+  console.log('Starting shuffling');
+
+  try {
+    var stringJSON = fs.readFileSync(PATH_TARGET, 'utf-8');
+  } catch(err) {
+    console.log(err);
+  }
+
+  var listJSON = JSON.parse(stringJSON);
+
+  if (!listJSON.stores.length) {
+    console.log('Array is empty');
+    return;
+  }
+
+  console.log('Target has ' + listJSON.stores.length + ' items');
+  var shuffledItems = shuffle(listJSON.stores);
+  var shuffledJSON = { stores: shuffledItems };
+
+  if (fs.existsSync(PATH_TARGET)) {
+    var stringShuffledJSON = JSON.stringify(shuffledJSON);
+
+    try {
+      fs.writeFileSync(PATH_TARGET, stringShuffledJSON);
+      console.log('Shuffling is complete');
+    } catch(err) {   
+      console.log(err);
+    };
+  }
+}
+
+startShuffle();
